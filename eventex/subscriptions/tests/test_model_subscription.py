d8aa4091681217d6ed6c1be228fc727ba50cf61d
lcnodc <lcno.dc@gmail.com>
2017-11-11 15:41:29 -0200

    Subscriptions persistency

---
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+from django.test import TestCase
+from eventex.subscriptions.models import Subscription
+
+
+class SubscriptionModelTest(TestCase):
+    def setUp(self):
+        self.obj = Subscription(
+            name = 'Luciano Cunha',
+            cpf = '12345678901',
+            email = 'luciano@email.com',
+            phone = '19-99999-9999',
+        )
+
+        self.obj.save()
+
+    def test_create(self):
+        self.assertTrue(Subscription.objects.exists())
+
+    def test_created_at(self):
+        """Subscription must have an auto created_at attr."""
+        self.assertIsInstance(self.obj.created_at, datetime)
