33dab271a27201ddf25840b2c1d4d396ec0dded5
lcnodc <lcno.dc@gmail.com>
2017-12-09 07:24:12 -0200

    Admim magic

--- eventex/subscriptions/tests/test_model_subscription.py
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -7,7 +7,7 @@ from eventex.subscriptions.models import Subscription
 class SubscriptionModelTest(TestCase):
     def setUp(self):
         self.obj = Subscription(
-            name = 'Luciano Cunha',
+            name = 'Luciano da Cunha',
             cpf = '12345678901',
             email = 'luciano@email.com',
             phone = '19-99999-9999',
@@ -21,3 +21,10 @@ class SubscriptionModelTest(TestCase):
     def test_created_at(self):
         """Subscription must have an auto created_at attr."""
         self.assertIsInstance(self.obj.created_at, datetime)
+
+    def test_str(self):
+        self.assertEqual('Luciano da Cunha', str(self.obj))
+
+    def test_paid_default_to_false(self):
+        """By default paid must be False."""
+        self.assertEqual(False, self.obj.paid)
